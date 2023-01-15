cedfb30f6e22574afc87926ec6a6d8adbd11100d
Lyza-Jorella <[EMAIL]>
2023-01-15 23:39:26 +0800

    add data in left subtree

--- Creating Binary Tree by using my full name.py
+++ Creating Binary Tree by using my full name.py
@@ -1,3 +1,20 @@
 print()
 print("**********Programmed by:**********")
 print("**********Lyza Jorella R. Del Rosario**********")
+
+class BinarySearchTreeNode:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return
+
+        if data < self.data:
+            # add data in left subtree
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
