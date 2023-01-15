7c3577fb9cd75e439160b202ac8066849e342052
Lyza-Jorella <[EMAIL]>
2023-01-16 01:04:45 +0800

    added the third code

--- Creating Binary Tree by using my full name.py
+++ Creating Binary Tree by using my full name.py
@@ -40,5 +40,24 @@ class BinarySearchTreeNode:
 
         return elements
 
+    def search(selfself, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
+
+
+
 
 
